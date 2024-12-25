b1312d9c734043ce4a1ce3b4429a68777211b448
Aru <aru24.m@gmail.com>
2024-12-25 16:42:13 -0500

    Writing CSV file to Database Objects and Displaying in UI

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    rent = models.IntegerField()
+    emi = models.IntegerField()
+    tax = models.IntegerField()
+    exp = models.IntegerField()
+    monthly_expenses = models.IntegerField(default=0)
+    monthly_income = models.IntegerField(default=0)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 import pandas as pd
 from pandas.errors import EmptyDataError
 import json
+from .models import Data
 
 
 # Create your views here.
@@ -25,14 +26,31 @@ def hello(request):
     try:
         if(request.method=='POST'):
             # print(request.FILES['file'])
+            history = Data.objects.all()
+            history.delete()
             filename = request.FILES['file']
             # print(pd.read_csv(filename))
             df = pd.read_csv(filename)
             json_records = df.reset_index().to_json(orient='records')
             data = []
             data = json.loads(json_records)
-            print(data)
+            # print(data)
             # print(json_records)
+            for d in data:
+                name = d['property_name']
+                price = d['property_price']
+                rent = d['property_rent']
+                emi = d['emi']
+                tax = d['tax']
+                exp = d['other_exp']
+                monthly_expenses = emi + tax + exp
+                monthly_income = rent - monthly_expenses
+                dt = Data(name=name,price=price,rent=rent,emi=emi,tax=tax,exp=exp,monthly_expenses=monthly_expenses,monthly_income=monthly_income)
+                dt.save()
+                # print('File Uploaded successfully')
+            data_objects = Data.objects.all()
+            context = {'data_objects' : data_objects}
+            return render(request, 'myapp/index.html', context)
         else:
             print('This is a GET request')
     except EmptyDataError:
