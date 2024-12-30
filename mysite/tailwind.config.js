1af61ac2dd02fe0ee4b4d3cbb4e914b891a7fe78
Aru <aru24.m@gmail.com>
2024-12-29 21:13:29 -0500

    Update to Formatting Index.html file

--- mysite/tailwind.config.js
+++ mysite/tailwind.config.js
@@ -1,13 +1,38 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./myapp/templates/myapp/index.html",
+  content: [
+    "./myapp/templates/myapp/index.html",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
 
   theme: {
-    extend: {},
-  },
+    extend: {
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          black: colors.black,
+          white: colors.white,
+          emerald: colors.emerald,
+          indigo: colors.indigo,
+          yellow: colors.yellow,
+          stone: colors.warmGray,
+          sky: colors.lightBlue,
+          neutral: colors.trueGray,
+          gray: colors.coolGray,
+          slate: colors.blueGray,
+          lime: colors.lime,
+          rose: colors.rose,
+        },
+    },
+},
   plugins: [],
 }
-
